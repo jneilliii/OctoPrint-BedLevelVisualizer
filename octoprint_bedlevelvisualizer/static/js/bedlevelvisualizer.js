128fd04c4dce3d06a865287e13d99980e4075c53
jneilliii <jneilliii+github@gmail.com>
2018-04-15 02:58:07 -0400

    test

--- octoprint_bedlevelvisualizer/static/js/bedlevelvisualizer.js
+++ octoprint_bedlevelvisualizer/static/js/bedlevelvisualizer.js
@@ -39,12 +39,12 @@ $(function () {
 
 		self.drawMesh = function (mesh_data) {
 			self.processing(false);
-			if(self.save_mesh()){
+/* 			if(self.save_mesh()){
 				if(!self.settingsViewModel.settings.plugins.bedlevelvisualizer.stored_mesh() || self.settingsViewModel.settings.plugins.bedlevelvisualizer.stored_mesh().length > 0){
 					self.settingsViewModel.settings.plugins.bedlevelvisualizer.stored_mesh(mesh_data);
 					self.settingsViewModel.saveData();
 				};
-			}
+			} */
 			OctoPrint.control.sendGcode('M155 S3');
 			var data = [{
 					z: mesh_data,
