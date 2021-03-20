083e0f155f89b56fbf5e0ca89cdece64c2bc8c44
jneilliii <jneilliii+github@gmail.com>
2021-03-20 12:54:32 -0400

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_bedlevelvisualizer"
 plugin_name = "Bed Visualizer"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.0.1"
+plugin_version = "1.1.0rc1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
